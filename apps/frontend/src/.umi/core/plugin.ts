0b1b45dc0b46332d64fd64d415a5320905fb0181
Zhaomian Zhao <loisisar@outlook.com>
2022-12-05 19:26:51 +0000

    TurboRepo Example Init

---
+++ apps/frontend/src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/lois/Documents/GitHub/example-monorepo-turborepo/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
+});
+
+export { plugin };
